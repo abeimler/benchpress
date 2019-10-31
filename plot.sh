9a8b918125704a5522cd09c145b930f84be73e32
Alex Beimler <alex-beimler@web.de>
2019-10-31 05:16:40 +0100

    add examples

--- plot.sh
+++ plot.sh
@@ -5,7 +5,11 @@
 ./build/src/examples/example_08 --plotdata --bench .*test.* --bench .*foo.* --bench .*bar.* > ./examples/example_08.dat
 
 ./build/src/examples/example_08 --plotdata --bench .*test.* --bench .*foo.* --bench .*bar.* --csvoutput ./examples/ --csvsuffix update
-./build/src/examples/example_09 --plotdata --bench ".*foo.*10M\\s+entities.*" --bench ".*bar.*10M\\s+entities.*" --csvoutput ./examples/ --csvsuffix 10Mentities
+./build/src/examples/example_09 --plotdata --bench ".*foo.*10M\\s+entities.*" --bench ".*bar.*10M\\s+entities.*" --csvoutput ./examples/ --csvsuffix 10Mentities --csvsort
+
+./build/src/examples/example_08 --plotdata --bench .*test.* --bench .*foo.* --bench .*bar.* --csvoutput ./examples/ --csvprefix update
+./build/src/examples/example_09 --plotdata --bench ".*foo.*10M\\s+entities.*" --bench ".*bar.*10M\\s+entities.*" --csvoutput ./examples/ --csvprefix 10Mentities
+./build/src/examples/example_08 --plotdata --bench .*test.* --bench .*foo.* --bench .*bar.* --csvoutput ./examples/ --csvprefix updates --csvunit seconds
 
 gnuplot ./plot06.plt
 gnuplot ./plot07.plt
