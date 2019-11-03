2648eea1aef4caa61678c92c04184e9591c7d69b
Alex Beimler <alex-beimler@web.de>
2019-11-03 03:10:54 +0100

    fix col sort

--- src/test/test_13.cpp
+++ src/test/test_13.cpp
@@ -11,53 +11,63 @@
 #include "utils.h"
 
 
-BENCHMARK("[10]  ccc      10 updates", [](benchpress::context* ctx) {
+BENCHMARK("[10]  ggggggg      10 updates", [](benchpress::context* ctx) {
     for (size_t i = 0; i < ctx->num_iterations(); ++i) {
         std::this_thread::sleep_for(std::chrono::milliseconds(1));
     }
 })
 
-BENCHMARK("[100]  ccc     100 updates", [](benchpress::context* ctx) {
+BENCHMARK("[100]  ggggggg     100 updates", [](benchpress::context* ctx) {
     for (size_t i = 0; i < ctx->num_iterations(); ++i) {
         std::this_thread::sleep_for(std::chrono::milliseconds(5));
     }
 })
 
-BENCHMARK("[500]  ccc     500 updates", [](benchpress::context* ctx) {
+BENCHMARK("[500]  ggggggg     500 updates", [](benchpress::context* ctx) {
     for (size_t i = 0; i < ctx->num_iterations(); ++i) {
         std::this_thread::sleep_for(std::chrono::milliseconds(10));
     }
 })
 
+BENCHMARK("[1000]  ggggggg     1000 updates", [](benchpress::context* ctx) {
+    for (size_t i = 0; i < ctx->num_iterations(); ++i) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(20));
+    }
+})
+BENCHMARK("[2000]  ggggggg     2000 updates", [](benchpress::context* ctx) {
+    for (size_t i = 0; i < ctx->num_iterations(); ++i) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(50));
+    }
+})
 
 
 
 
-BENCHMARK("[10]  zzzzz      10 updates", [](benchpress::context* ctx) {
+BENCHMARK("[10]  eeeeee1      10 updates", [](benchpress::context* ctx) {
     for (size_t i = 0; i < ctx->num_iterations(); ++i) {
         std::this_thread::sleep_for(std::chrono::milliseconds(30));
     }
 })
 
-BENCHMARK("[100]  zzzzz     100 updates", [](benchpress::context* ctx) {
+BENCHMARK("[100]  eeeeee1     100 updates", [](benchpress::context* ctx) {
     for (size_t i = 0; i < ctx->num_iterations(); ++i) {
         std::this_thread::sleep_for(std::chrono::milliseconds(53));
     }
 })
 
-BENCHMARK("[500]  zzzzz     500 updates", [](benchpress::context* ctx) {
+BENCHMARK("[500]  eeeeee1     500 updates", [](benchpress::context* ctx) {
     for (size_t i = 0; i < ctx->num_iterations(); ++i) {
         std::this_thread::sleep_for(std::chrono::milliseconds(70));
     }
 })
 
-BENCHMARK("[1000] zzzzz    1000 updates", [](benchpress::context* ctx) {
+BENCHMARK("[1000] eeeeee1    1000 updates", [](benchpress::context* ctx) {
     for (size_t i = 0; i < ctx->num_iterations(); ++i) {
         std::this_thread::sleep_for(std::chrono::milliseconds(96));
     }
 })
 
-BENCHMARK("[2000] zzzzz    2000 updates", [](benchpress::context* ctx) {
+BENCHMARK("[2000] eeeeee1    2000 updates", [](benchpress::context* ctx) {
     for (size_t i = 0; i < ctx->num_iterations(); ++i) {
         std::this_thread::sleep_for(std::chrono::milliseconds(83));
     }
@@ -65,85 +75,105 @@ BENCHMARK("[2000] zzzzz    2000 updates", [](benchpress::context* ctx) {
 
 
 
-BENCHMARK("[10]  xy      10 updates", [](benchpress::context* ctx) {
+BENCHMARK("[10]  eeff      10 updates", [](benchpress::context* ctx) {
     for (size_t i = 0; i < ctx->num_iterations(); ++i) {
         std::this_thread::sleep_for(std::chrono::milliseconds(35));
     }
 })
 
-BENCHMARK("[100]  xy     100 updates", [](benchpress::context* ctx) {
+BENCHMARK("[100]  eeff     100 updates", [](benchpress::context* ctx) {
     for (size_t i = 0; i < ctx->num_iterations(); ++i) {
         std::this_thread::sleep_for(std::chrono::milliseconds(58));
     }
 })
 
-BENCHMARK("[500]  xy     500 updates", [](benchpress::context* ctx) {
+BENCHMARK("[500]  eeff     500 updates", [](benchpress::context* ctx) {
     for (size_t i = 0; i < ctx->num_iterations(); ++i) {
         std::this_thread::sleep_for(std::chrono::milliseconds(75));
     }
 })
 
-BENCHMARK("[1000] xy    1000 updates", [](benchpress::context* ctx) {
+BENCHMARK("[1000] eeff    1000 updates", [](benchpress::context* ctx) {
     for (size_t i = 0; i < ctx->num_iterations(); ++i) {
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
 })
 
-BENCHMARK("[2000] xy    2000 updates", [](benchpress::context* ctx) {
+BENCHMARK("[2000] eeff    2000 updates", [](benchpress::context* ctx) {
     for (size_t i = 0; i < ctx->num_iterations(); ++i) {
         std::this_thread::sleep_for(std::chrono::milliseconds(96));
     }
 })
 
 
-BENCHMARK("[10]  aa      10 updates", [](benchpress::context* ctx) {
+BENCHMARK("[10]  eeeeee2      10 updates", [](benchpress::context* ctx) {
     for (size_t i = 0; i < ctx->num_iterations(); ++i) {
         std::this_thread::sleep_for(std::chrono::milliseconds(1));
     }
 })
 
-BENCHMARK("[100]  aa     100 updates", [](benchpress::context* ctx) {
+BENCHMARK("[100]  eeeeee2     100 updates", [](benchpress::context* ctx) {
     for (size_t i = 0; i < ctx->num_iterations(); ++i) {
         std::this_thread::sleep_for(std::chrono::milliseconds(2));
     }
 })
 
-BENCHMARK("[500]  aa     500 updates", [](benchpress::context* ctx) {
+BENCHMARK("[500]  eeeeee2     500 updates", [](benchpress::context* ctx) {
     for (size_t i = 0; i < ctx->num_iterations(); ++i) {
         std::this_thread::sleep_for(std::chrono::milliseconds(3));
     }
 })
 
-BENCHMARK("[1000] aa    1000 updates", [](benchpress::context* ctx) {
+BENCHMARK("[1000] eeeeee2    1000 updates", [](benchpress::context* ctx) {
     for (size_t i = 0; i < ctx->num_iterations(); ++i) {
         std::this_thread::sleep_for(std::chrono::milliseconds(4));
     }
 })
 
-BENCHMARK("[10]  bbbb      10 updates", [](benchpress::context* ctx) {
+
+
+BENCHMARK("[10]  aaaa      10 updates", [](benchpress::context* ctx) {
     for (size_t i = 0; i < ctx->num_iterations(); ++i) {
-        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+        std::this_thread::sleep_for(std::chrono::milliseconds(50));
     }
 })
 
-BENCHMARK("[100]  bbbb     100 updates", [](benchpress::context* ctx) {
+BENCHMARK("[100]  aaaa     100 updates", [](benchpress::context* ctx) {
     for (size_t i = 0; i < ctx->num_iterations(); ++i) {
-        std::this_thread::sleep_for(std::chrono::milliseconds(2));
+        std::this_thread::sleep_for(std::chrono::milliseconds(53));
     }
 })
 
-BENCHMARK("[500]  bbbb     500 updates", [](benchpress::context* ctx) {
+BENCHMARK("[500]  aaaa     500 updates", [](benchpress::context* ctx) {
     for (size_t i = 0; i < ctx->num_iterations(); ++i) {
-        std::this_thread::sleep_for(std::chrono::milliseconds(3));
+        std::this_thread::sleep_for(std::chrono::milliseconds(80));
     }
 })
 
-BENCHMARK("[1000] bbbb    1000 updates", [](benchpress::context* ctx) {
+
+BENCHMARK("[10]  aaaaaa      10 updates", [](benchpress::context* ctx) {
     for (size_t i = 0; i < ctx->num_iterations(); ++i) {
-        std::this_thread::sleep_for(std::chrono::milliseconds(4));
+        std::this_thread::sleep_for(std::chrono::milliseconds(50));
     }
 })
 
+BENCHMARK("[100]  aaaaaa     100 updates", [](benchpress::context* ctx) {
+    for (size_t i = 0; i < ctx->num_iterations(); ++i) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(53));
+    }
+})
+
+BENCHMARK("[500]  aaaaaa     500 updates", [](benchpress::context* ctx) {
+    for (size_t i = 0; i < ctx->num_iterations(); ++i) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(80));
+    }
+})
+
+BENCHMARK("[1000]  aaaaaa     1000 updates", [](benchpress::context* ctx) {
+    for (size_t i = 0; i < ctx->num_iterations(); ++i) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+})
 
 
 
@@ -152,7 +182,7 @@ SCENARIO( "multible benchmarks with headers, plotdata, different order and tags;
     GIVEN( "default benchmark options with headers and plotdata" ) {
         benchpress::options bench_opts;
         bench_opts
-            .bench( { ".*aa.*update.*", ".*bbbb.*update.*", ".*zzzzz.*update.*", ".*ccc.*update.*", ".*xy.*update.*" } )
+            .bench( { ".*aaaa.*update.*", ".*aaaaaa.*update.*", ".*eeff.*update.*", ".*eeeeee1.*update.*", ".*eeeeee2.*update.*", ".*ggggggg.*update.*" } )
             .plotdata(true)
             .csvoutput("./");
 
@@ -172,7 +202,7 @@ SCENARIO( "multible benchmarks with headers, plotdata, different order and tags;
             std::vector<std::string> fields_vector (fields.size());
             std::copy(std::begin(fields), std::end(fields), std::begin(fields_vector));
 
-            REQUIRE( results.size() == 21 );
+            REQUIRE( results.size() == 30 );
 
             auto csv_results = benchpress::make_csv(bench_opts, std::get<1>(res));
 
@@ -188,10 +218,64 @@ SCENARIO( "multible benchmarks with headers, plotdata, different order and tags;
 
             THEN ( "headers are in the same order" ) {
                 REQUIRE(csv_results_00_lines.size() >= 1);
-                REQUIRE(output_lines.size() >= 24);
+                REQUIRE(output_lines.size() >= 33);
 
-                REQUIRE_THAT( csv_results_00_lines[0], Matches( fmt::format(";.{}.;.{}.;.{}.;.{}.;.{}.", "aaupdate", "xyupdate", "cccupdate", "bbbbupdate", "zzzzzupdate") ) );
-                REQUIRE_THAT( output_lines[23], Matches( fmt::format("\\s+{}\\s+{}\\s+{}\\s+{}\\s+{}", "aaupdate", "xyupdate", "cccupdate", "bbbbupdate", "zzzzzupdate") ) );
+                REQUIRE_THAT( csv_results_00_lines[0], Matches( fmt::format(";.{}.;.{}.;.{}.;.{}.;.{}.;.{}.", "aaaaupdate", "eeffupdate", "aaaaaaupdate", "eeeeee1update", "eeeeee2update", "gggggggupdate") ) );
+                REQUIRE_THAT( output_lines[32], Matches( fmt::format("\\s+{}\\s+{}\\s+{}\\s+{}\\s+{}\\s+{}", "aaaaupdate", "eeffupdate", "aaaaaaupdate", "eeeeee1update", "eeeeee2update", "gggggggupdate") ) );
+            }
+
+        }
+    }
+
+}
+
+
+SCENARIO( "multible benchmarks with headers, plotdata, different order and tags; print csv and plotdata, headers are in the same order with csvsort", "[benchmark][csv]" ) {
+
+    GIVEN( "default benchmark options with headers and plotdata" ) {
+        benchpress::options bench_opts;
+        bench_opts
+            .bench( { ".*aaaa.*update.*", ".*aaaaaa.*update.*", ".*eeff.*update.*", ".*eeeeee1.*update.*", ".*eeeeee2.*update.*", ".*ggggggg.*update.*" } )
+            .plotdata(true)
+            .csvsort(true)
+            .csvoutput("./");
+
+        WHEN( "run benchmark" ) {
+            using Catch::Matchers::Matches;
+
+            auto res = benchpress::run_benchmarks_details(bench_opts);
+            std::string output = std::get<0>(res);
+            auto results = std::get<1>(res).results;
+            auto headers = std::get<1>(res).headers;
+            auto fields = std::get<1>(res).fields;
+            auto results_map = std::get<1>(res).results_map;
+
+            std::vector<std::string> headers_vector (headers.size());
+            std::copy(std::begin(headers), std::end(headers), std::begin(headers_vector));
+
+            std::vector<std::string> fields_vector (fields.size());
+            std::copy(std::begin(fields), std::end(fields), std::begin(fields_vector));
+
+            REQUIRE( results.size() == 30 );
+
+            auto csv_results = benchpress::make_csv(bench_opts, std::get<1>(res));
+
+            REQUIRE( csv_results.size() == 1 );
+
+            auto csv_results00_content_lines = split_string(csv_results[0].content);
+
+            CAPTURE(output);
+            CAPTURE(csv_results[0].content);
+
+            auto csv_results_00_lines = split_string(csv_results[0].content);
+            auto output_lines = split_string(output);
+
+            THEN ( "headers are in the same order" ) {
+                REQUIRE(csv_results_00_lines.size() >= 1);
+                REQUIRE(output_lines.size() >= 33);
+
+                REQUIRE_THAT( csv_results_00_lines[0], Matches( fmt::format(";.{}.;.{}.;.{}.;.{}.;.{}.;.{}.", "aaaaupdate", "eeffupdate", "aaaaaaupdate", "eeeeee1update", "eeeeee2update", "gggggggupdate") ) );
+                REQUIRE_THAT( output_lines[32], Matches( fmt::format("\\s+{}\\s+{}\\s+{}\\s+{}\\s+{}\\s+{}", "aaaaupdate", "eeffupdate", "aaaaaaupdate", "eeeeee1update", "eeeeee2update", "gggggggupdate") ) );
             }
 
         }
